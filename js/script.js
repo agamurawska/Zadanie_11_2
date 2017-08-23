8e5ebfe9fd59f7c1460b295801cb8fd8b42d324d
aga <aga.murawska@gmail.com>
2017-08-23 23:40:28 +0200

    this and self excercise in js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+﻿
+    function Button(text) {
+    this.text = text || "Hallo";
+}
+
+    Button.prototype = {
+        create: function() {
+            this.$element =$('<button>');
+            this.$element.text(this.text);
+            this.$element.click(function(){
+                alert(self.text)};
+            this.$element.appendTo($('body'));
+        }
+        };
+    
+    var btn1 = new Button('Hallo');
+    btn1.create();
